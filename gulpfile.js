beea44bb74cb15502e03f03a835cd31c09ef0620
jgluhov <jgluhov@gmail.com>
2015-11-11 18:10:21 +0300

    add button and fix form z-index.

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,19 @@ var gulp = require('gulp'),
   stylus = require('gulp-stylus'),
   nib = require('nib'),
   browserSync = require('browser-sync').create(),
-  less = require('gulp-less');
+  less = require('gulp-less'),
+  plumber = require('gulp-plumber');
 
 gulp.task('js', function () {
   gulp.src('./src/js/*.js')
+    .pipe(plumber())
     .pipe(gulp.dest('./assets/js'))
     .pipe(browserSync.stream());
 });
 
 gulp.task('less', function () {
   gulp.src('./src/less/uikit.less')
+    .pipe(plumber())
     .pipe(less())
     .pipe(gulp.dest('./assets/css'))
     .pipe(browserSync.stream());
@@ -19,6 +22,7 @@ gulp.task('less', function () {
 
 gulp.task('styl', function () {
   gulp.src('./src/styl/app.styl')
+    .pipe(plumber())
     .pipe(stylus({use: nib(), import: 'nib'}))
     .pipe(gulp.dest('./assets/css'))
     .pipe(browserSync.stream());
@@ -29,9 +33,10 @@ gulp.task('serve', ['js','less','styl'], function() {
     proxy: "localhost:3000"
   });
 
+  gulp.watch("templates/*.jade", browserSync.reload);
   gulp.watch("src/js/*.js", ['js']);
   gulp.watch("src/less/*.less", ['less']);
-  gulp.watch("src/styl/*.styl", ['styl']);
+  gulp.watch("src/styl/**/*.styl", ['styl']);
 });
 
 gulp.task('default', ['serve']);
